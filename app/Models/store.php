6a843e803a15fc498f96f637de790a0c5f88c045
dineshmuthukumar <42163115+dineshmuthukumar@users.noreply.github.com>
2024-05-18 15:12:13 +0530

    checking

--- app/Models/store.php
+++ app/Models/store.php
@@ -14,7 +14,7 @@ class store extends Model
     protected $primaryKey = 'id';
     
     protected $fillable = [
-       'id', 'user_id', 'name', 'contact_person_name', 'phone', 'mobile_number', 'email', 'gst_number',
+       'id', 'name', 'contact_person_name', 'phone', 'mobile_number', 'email', 'gst_number',
         'drug_licence', 'address', 'area', 'state', 'city',"pincode","store_image","store_logo","bank_name","bank_account_number","ifsc_code","app_status_id","status_id"
     ];
 
