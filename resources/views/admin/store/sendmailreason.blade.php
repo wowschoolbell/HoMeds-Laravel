88b3252e835356dbf0c2d0e9550852526eb46cca
dineshmuthukumar <42163115+dineshmuthukumar@users.noreply.github.com>
2024-05-24 16:49:34 +0530

    fixes

--- resources/views/admin/store/sendmailreason.blade.php
+++ resources/views/admin/store/sendmailreason.blade.php
@@ -41,7 +41,7 @@ font-weight:bold;
 
 <h3>Dear {{$name}}</h3>
 
-<h4>The store account is currently on {{strtolower($status)}}due to the following reason: {{$reason}}</h4>
+<h4>The store account is currently on {{strtolower($status)}} due to the following reason: {{$reason}}</h4>
 
 <h4>If you have any questions or would like to activate your account, please contact our Renewal Team for assistance:</h4>
 <h3>The HoMEds Team</h3>
